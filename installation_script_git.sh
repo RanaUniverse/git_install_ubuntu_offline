fe83df8bfe094f981ccd13ef2016dc02e0cf219e
RanaUniverse <RanaUniverse321@gmail.com>
2025-06-04 12:50:33 +0530

    installation script got updated with apt

--- installation_script_git.sh
+++ installation_script_git.sh
@@ -1,6 +1,7 @@
 echo "This is installing 'git' in this machine."
 
-sudo dpkg -i dependencies/*.deb
+# sudo dpkg -i dependencies/*.deb
+sudo apt install ./dependencies/*.deb
 
 echo "Git has been installed, Now you can use git command in this machine by terminal, for more git power use sudo apt install git-all"
 
